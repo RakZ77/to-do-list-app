1eee99b8c6059c6f33fdf4a1b5c6dfda276dee25
Vahrak <vahraksay@gmail.com>
2026-03-25 23:51:10 +0700

    Adding new add activity and fragments UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,10 +47,12 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.picasso)
+    implementation(libs.viewpager2)
     testImplementation(libs.junit)
     implementation("androidx.datastore:datastore-preferences-rxjava3:1.2.0")
     implementation("io.reactivex.rxjava3:rxjava:3.1.6")
     implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
+    implementation("com.google.android.material:material:1.11.0")
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
